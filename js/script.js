dd10184f6728e478590cfd0bffb391425b270ead
Obi Nwokogba <83872952+obi-nwokogba@users.noreply.github.com>
2021-08-04 20:52:19 -0400

    UI Improvements and Android APK Link

--- js/script.js
+++ js/script.js
@@ -2,16 +2,21 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
 const URL_TRENDING = "search/trending";
 
+function truncateString(string, limit) {
+    if (string.length > limit) {
+      return string.substring(0, limit) + "..."
+    } else {
+      return string
+    }
+  }
+
 let $mainContainer = $(".mainContainer");
 
 // This function is called when someone seearches for a movie.
 function loadTrendingCryptos() {
 
-    let tendingCryptoName, trendingCryptoSymbol, trendingCryptoCoinID, trendingCryptoPriceBTC,
-        trendingCryptoMarketCapRank;
-
     let trendingCryptosHTML = `<h2>7 Most Popular Cryptocurrencies Today</h2>
-    <br /><div class="trendingCryptoContainer">`;
+    <div class="trendingCryptoContainer">`;
 
     $.ajax({
             url: `${BASE_URL}/${URL_TRENDING}`
@@ -26,9 +31,11 @@ function loadTrendingCryptos() {
                     let trendingCryptoSymbol = returnedData.coins[i].item.symbol;
                     let trendingCryptoMarketCapRank = returnedData.coins[i].item.market_cap_rank;
 
-                    trendingCryptosHTML = trendingCryptosHTML + 
-                    `<div class="trendingCryptoBox"> ${trendingCryptoName} &middot; ${trendingCryptoSymbol}
-                    <h1>${trendingCryptoMarketCapRank}</h1>
+                    trendingCryptosHTML = trendingCryptosHTML +
+                        `<div class="trendingCryptoBox"> ${trendingCryptoSymbol} &middot; ${truncateString(trendingCryptoName,12)}
+
+                    <br /><span class="verylight">Market Cap Rank</span><br />
+                    <span class="bignumber1">${trendingCryptoMarketCapRank}</span>
                     </div>`;
                 }
 
@@ -39,11 +46,54 @@ function loadTrendingCryptos() {
             }
         );
 }
-
 loadTrendingCryptos();
 
 
+function getCryptoInformationBox(cryptoID) {
 
+    let thisCryptoName, thisCryptoSymbol, thisCryptoCoinID, thisCryptoPriceBTC,
+        thisCryptoMarketCapRank;
+    let thisCryptoHTML = `<div class="trendingCryptoContainer">`;
+
+    $.ajax({
+            url: `https://api.coingecko.com/api/v3/coins/bitcoin?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true`
+        })
+        .then(
+            function (returnedData) {
+
+                thisCryptoName = returnedData.name;
+                thisCryptoSymbol = returnedData.symbol;
+                thisCryptoMarketCapRank = returnedData.market_cap_rank;
+                trendingCryptoHTML =
+                    `<div class="trendingCryptoBox"> ${thisCryptoName} &middot; ${thisCryptoSymbol}
+
+                <br /><span class="verylight">Market Cap Rank</span><br />
+                <span class="bignumber1">${trendingCryptoMarketCapRank}</span>
+                </div>`;
+                return trendingCryptoHTML;
+            },
+            function (error) {
+                console.log("bad request: ", error);
+            }
+        );
+}
+
+function hallOfFameCryptos() {
+
+    let trendingCryptosHTML = `<h2>Popular Cyptocurrencies</h2>
+    <div class="trendingCryptoContainer">`;
+
+    let mostLovedCryptos = [`bitcoin`, `ethereum`, `EOS`];
+    let cryptoHTML = "";
+    let thisCryptoBox = "";
+
+    for (i = 0; i < mostLovedCryptos.length; i++) {
+        thisCryptoBox = getCryptoInformationBox(mostLovedCryptos[i]);
+        $mainContainer.append(thisCryptoBox);
+    }
+}
+
+hallOfFameCryptos();
 
 
 
@@ -63,18 +113,5 @@ loadTrendingCryptos();
 /*
 FINNHUB API KEY
 c2qq5lqad3ickc1m1gsg
-
-…or create a new repository on the command line
-echo "# pesapulse" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/obi-nwokogba/pesapulse.git
-git push -u origin main
-…or push an existing repository from the command line
-git remote add origin https://github.com/obi-nwokogba/pesapulse.git
-git branch -M main
-git push -u origin main
 
 */
